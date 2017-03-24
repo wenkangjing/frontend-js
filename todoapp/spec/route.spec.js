d6379edf5dfce3622d89ef5ac61e265a23e1f605
Jamie Wen <wenkangjing@gmail.com>
2017-03-24 23:50:21 +1100

    Add test cases on route

--- todoapp/spec/route.spec.js
+++ todoapp/spec/route.spec.js
@@ -1,12 +1,22 @@
 var request = require("request");
 var root = "http://localhost:3000";
 
-
-xdescribe("index", function() {
+describe("index", function() {
   it("returns an array of albums", function(done) {
-    request.get(getURL(), function(error, response, body) {
+    request.get(root, function(error, response, body) {
       expect(response.toJSON().statusCode).toEqual(200);
       done();
     });
   });
+  it("save an array of albums", function(done) {
+    var data = {
+      body: {
+        todos: JSON.stringify([ {title: "testcase 1"},  {title: "testcase 2"} ])
+      }
+    };
+    request.post(root + "/todos",  JSON.stringify(data), function(error, response, body) {
+      expect(response).toEqual(200);
+      done();
+    });
+  });  
 });

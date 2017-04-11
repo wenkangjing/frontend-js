e7b5916007a9fe886b7457ea35b296538a7d209f
Jamie Wen <wenkangjing@gmail.com>
2017-04-11 12:34:08 +1000

    Notification: show activities of subscribed

--- trello_lite/public/javascripts/views/popovers/notification_popover.js
+++ trello_lite/public/javascripts/views/popovers/notification_popover.js
@@ -7,7 +7,12 @@ var NotificationView = Backbone.View.extend({
     this.remove();
   },
   render: function(e) {
-    this.$el.html(this.template({activities: App.activities.toJSON()}));
+    var activities = App.activities.toJSON().filter(function(act) {
+      var card = App.cards.get(act.idCard);
+      var list = App.lists.get(card.get("idList"));
+      return card.get("subscribed") || list.get("subscribed");
+    });
+    this.$el.html(this.template({activities: activities}));
     this.$el.appendTo(App.$el);
   },
   initialize: function() {
